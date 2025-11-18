20d215a73214d58572f8ebd3db020796cba57cc8
zeroheight <tokens-management@zeroheight.com>
2025-11-18 10:01:25 +0100

    Update Foundations from zeroheight

---
+++ build/ios/DesignTokens.swift/token_Brand_Business.swift
@@ -0,0 +1,18 @@
+
+//
+// 
+//
+
+import UIKit
+
+public class Foundations {
+    public static let colorBrandBase = UIColor(red: 0.039, green: 0.039, blue: 0.039, alpha: 1)
+    public static let colorBrandBaseAlpha = UIColor(red: 0.090, green: 0.090, blue: 0.090, alpha: 0)
+    public static let colorBrandBaseAlphaLight = UIColor(red: 0.976, green: 0.976, blue: 0.976, alpha: 0)
+    public static let colorBrandDark = UIColor(red: 0.039, green: 0.039, blue: 0.039, alpha: 1)
+    public static let colorBrandDarker = UIColor(red: 0.039, green: 0.039, blue: 0.039, alpha: 1)
+    public static let colorBrandLight = UIColor(red: 0.976, green: 0.976, blue: 0.976, alpha: 1)
+    public static let colorBrandLighter = UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1)
+    public static let colorBrandLightest = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+    public static let colorOnBrandDark = UIColor(red: 0.090, green: 0.090, blue: 0.090, alpha: 1)
+}
