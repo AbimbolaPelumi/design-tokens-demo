d615b0d79b5130a83c9633ed88c807819d222fb2
zeroheight <tokens-management@zeroheight.com>
2025-11-18 10:01:21 +0100

    Update Foundations from zeroheight

---
+++ build/android/DesignTokens.kt/token_Brand_Retail.kt
@@ -0,0 +1,19 @@
+
+
+
+package ;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object  {
+  val colorBrandBase = Color(0xff40196d)
+  val colorBrandBaseAlpha = Color(0x0040196d)
+  val colorBrandBaseAlphaLight = Color(0x004f2088)
+  val colorBrandDark = Color(0xff40196d)
+  val colorBrandDarker = Color(0xff2c1647)
+  val colorBrandLight = Color(0xff4f2088)
+  val colorBrandLighter = Color(0xff672ab2)
+  val colorBrandLightest = Color(0x3340196d)
+  val colorOnBrandDark = Color(0xffffffff)
+}
